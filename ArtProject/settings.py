3a502d58bec5c763ddd1a2a2afd76c35478cc294
Elias <afloresp3@unemi.edu.ec>
2025-04-17 19:46:22 -0500

    Requerimientos

--- ArtProject/settings.py
+++ ArtProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Aplicaciones.Header',
-    'Aplicaciones.Login',
+    'Aplicaciones.Usuarios',
 ]
 
 MIDDLEWARE = [
